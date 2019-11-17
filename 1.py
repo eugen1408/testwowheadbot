8b9705dd6e5a282835d56032c48abec88271c993
evaganov <evaganov@outlook.com>
2019-11-17 19:25:19 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,6 +4,6 @@ import urllib.request
 
 if __name__ == "__main__":
     contents = urllib.request.urlopen("https://ru.wowhead.com/world-quests/bfa/eu").read().decode('utf-8')
-    id = "56007"
+    id = "56017"
     if id in contents:
         urllib.request.urlopen("https://api.telegram.org/bot343228570:AAHWvNfSyL-O5QgnkQXWK7DDTWL70_XHTcw/sendMessage?chat_id=129934507&text=https://ru.wowhead.com/quest=%s" % id)
